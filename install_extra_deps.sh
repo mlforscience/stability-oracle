a403f00c885d16aee7a7635b3f40d569cbdf331d
Joseph <joseph@viviano.ca>
2023-11-30 17:04:07 -0500

    sync

--- install_extra_deps.sh
+++ install_extra_deps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+conda install pip
 pip install gitpython  # Required for installation of inverse fold.
 conda install pyg -c pyg -c conda-forge
+conda install pytorch-sparse -c pyg
 #conda install cudatoolkit=11.3 -c pytorch
